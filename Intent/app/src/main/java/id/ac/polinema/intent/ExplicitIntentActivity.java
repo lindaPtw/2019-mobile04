ab74c850778dc63b0a308863442d68dd65157c51
lindaPtw <lindapuspitatw@gmail.com@email.com>
2019-10-26 15:17:49 +0700

    Melengkapi ExplicitIntentActivity sehingga bisa mengambil nilai pada EditText kemudian menampilkannya pada TextView.

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -8,11 +8,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
+    public EditText inputName;
+    public TextView outputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        inputName = findViewById(R.id.input_name);
+        outputText = findViewById(R.id.text_output);
+    }
+
+    public void handleSubmit(View view) {
+        String name = inputName.getText().toString();
+        outputText.setText("Hi " +name);
     }
 
 }
